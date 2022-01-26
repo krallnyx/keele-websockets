0ec2b6e44f991e1973fcae78fab0b4e3c301d356
Stu Watts <stu.watts@gmail.com>
2022-01-26 21:22:43 +0000

    working Dockerfile

--- webapp.py
+++ webapp.py
@@ -1,11 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Welcome to CSC40088 Fundamentals of Comp Sci'
 
+
 app.run(host='0.0.0.0', port=8081)
-
-
-
