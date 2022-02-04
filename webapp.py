47840792450a8da2e428ce6fd19a66abec2b663a
Stu Watts <stu.watts@gmail.com>
2022-02-04 12:47:09 +0000

    Week4

--- webapp.py
+++ webapp.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
 import csv
 import itertools
-
+ 
 app = Flask(__name__)
-
+ 
 
 @app.route('/')
 def index():
     return 'Welcome to CSC40088 Fundamentals of Comp Sci'
 
 
+@app.route('/advanced')
+def display_data_d3():
+    return render_template("advanced.html")
+
+
 @app.route('/basic')
 def display_data():
-    with open('data/Kaggle_TwitterUSAirlineSentiment.csv', encoding='utf-8-sig') as csv_file:
+    with open('static/data/Kaggle_TwitterUSAirlineSentiment.csv', encoding='utf-8-sig') as csv_file:
         data = csv.reader(csv_file, delimiter=',')
         first_line = True
         tweetData = []
@@ -27,8 +32,9 @@ def display_data():
                 })
             else:
                 first_line = False
-        sorted_tweetData = sorted(tweetData, key = lambda i: i['airline_sentiment_confidence'])
+        sorted_tweetData = sorted(tweetData, key=lambda i: i['airline_sentiment_confidence'])
         return render_template("basic.html", tweetData=sorted_tweetData)
 
 
 app.run(host='0.0.0.0', port=8080)
+
